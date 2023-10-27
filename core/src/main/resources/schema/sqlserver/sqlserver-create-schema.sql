4c5eab8b69ae209adc09d4e22925c334f020c077
AndyChen <iRoiocam@gmail.com>
2023-10-27 20:01:06 +0800

    Replace event_tag FK to get rid of insert and return #710 (#731)
    * Replace event_tag FK to get rid of insert and return #710
    
    * support rolling updates #710
    
    * remove CRLF #710
    
    * optimized migrator #710
    
    * fixes oracle test #710
    
    * unitTest,SQL for migration #710
    
    * fix MigratorSpec #710
    
    * chore: typo fix #710
    
    * fix: IntegrationTest and clean code #710
    
    * fix: compatible legacy tag read #673
    
    * chore: mi-ma filter for PR
    
    * fix: optimized migrate step
    
    * fix: dialect for column fill
    
    * fix: update migration sql
    
    * fix: mysql dialect
    
    * fix: dialect syntax
    
    * fix: dialect syntax
    
    * fix: avoid use system table of mysql
    
    * fix: batch insert caused flaky test
    
    * fix: insert less event of large batch
    
    * fix: script fix and strongly express two-step update

--- core/src/main/resources/schema/sqlserver/sqlserver-create-schema.sql
+++ core/src/main/resources/schema/sqlserver/sqlserver-create-schema.sql
@@ -1,41 +1,43 @@
-CREATE TABLE event_journal(
+CREATE TABLE event_journal (
     "ordering" BIGINT IDENTITY(1,1) NOT NULL,
     "deleted" BIT DEFAULT 0 NOT NULL,
-    "persistence_id" VARCHAR(255) NOT NULL,
+    "persistence_id" NVARCHAR(255) NOT NULL,
     "sequence_number" NUMERIC(10,0) NOT NULL,
-    "writer" VARCHAR(255) NOT NULL,
+    "writer" NVARCHAR(255) NOT NULL,
     "write_timestamp" BIGINT NOT NULL,
-    "adapter_manifest" VARCHAR(MAX) NOT NULL,
+    "adapter_manifest" NVARCHAR(MAX) NOT NULL,
     "event_payload" VARBINARY(MAX) NOT NULL,
     "event_ser_id" INTEGER NOT NULL,
-    "event_ser_manifest" VARCHAR(MAX) NOT NULL,
+    "event_ser_manifest" NVARCHAR(MAX) NOT NULL,
     "meta_payload" VARBINARY(MAX),
     "meta_ser_id" INTEGER,
-    "meta_ser_manifest" VARCHAR(MAX)
+    "meta_ser_manifest" NVARCHAR(MAX)
     PRIMARY KEY ("persistence_id", "sequence_number")
 );
 
 CREATE UNIQUE INDEX event_journal_ordering_idx ON event_journal(ordering);
 
 CREATE TABLE event_tag (
-    "event_id" BIGINT NOT NULL,
-    "tag" VARCHAR(255) NOT NULL
-    PRIMARY KEY ("event_id","tag")
+    "event_id" BIGINT,
+    "persistence_id" NVARCHAR(255),
+    "sequence_number" NUMERIC(10,0),
+    "tag" NVARCHAR(255) NOT NULL
+    PRIMARY KEY ("persistence_id", "sequence_number","tag"),
     constraint "fk_event_journal"
-        foreign key("event_id")
-        references "dbo"."event_journal"("ordering")
+        foreign key("persistence_id", "sequence_number")
+        references "dbo"."event_journal"("persistence_id", "sequence_number")
         on delete CASCADE
 );
 
 CREATE TABLE "snapshot" (
-    "persistence_id" VARCHAR(255) NOT NULL,
+    "persistence_id" NVARCHAR(255) NOT NULL,
     "sequence_number" NUMERIC(10,0) NOT NULL,
     "created" BIGINT NOT NULL,
     "snapshot_ser_id" INTEGER NOT NULL,
-    "snapshot_ser_manifest" VARCHAR(255) NOT NULL,
+    "snapshot_ser_manifest" NVARCHAR(255) NOT NULL,
     "snapshot_payload" VARBINARY(MAX) NOT NULL,
     "meta_ser_id" INTEGER,
-    "meta_ser_manifest" VARCHAR(255),
+    "meta_ser_manifest" NVARCHAR(255),
     "meta_payload" VARBINARY(MAX),
     PRIMARY KEY ("persistence_id", "sequence_number")
   )
